016a268be5da9daf5d772a02c0138962e2d6a523
Sarahkh4 <sarahkhan8040@gmail.com>
2026-05-19 19:24:07 +0500

    code changed

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from textqueue!")
+from fastapi import FastAPI
+from src.task import generate_paragraph
+from celery.result import AsyncResult
+from src.celery_worker import celery
 
+app = FastAPI()
+@app.get("/")
+def home():
+    return {"message": "Paragraph Generator API"}
+@app.post("/generate/{topic}")
+def generate(topic: str):
 
-if __name__ == "__main__":
-    main()
+    task = generate_paragraph.delay(topic)
+
+    return {
+        "task_id": task.id,
+        "status": "Processing started"
+    }
+@app.get("/result/{task_id}")
+def get_result(task_id: str):
+    task = AsyncResult(task_id, app=celery)
+    return {
+        "task_id": task.id,
+        "status": task.status,
+        "result": task.result
+    }
